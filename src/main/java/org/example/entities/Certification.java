8777a37e93a2e83ae5cd484ff9d10495c75fa1f3
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-21 20:00:30 +0530

    bidirectional many to many relationship

--- src/main/java/org/example/entities/Certification.java
+++ src/main/java/org/example/entities/Certification.java
@@ -2,6 +2,8 @@ package org.example.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Certification {
     @Id
@@ -11,6 +13,21 @@ public class Certification {
 
     @Column(name = "CERTIFICATION_NAME")
     private String certificationName;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "JOIN_STUDENT_CERTIFICATION",
+            joinColumns =  {@JoinColumn(name = "CERTIFICATION_ID_FK") },
+            inverseJoinColumns = {@JoinColumn(name = "STUDENT_ID_FK")}
+    )
+    private Set<Student> students;
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
 
     public Long getCertificationId() {
         return certificationId;
