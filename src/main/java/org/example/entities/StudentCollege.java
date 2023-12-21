58e3b1526c1f5045e545565e934fd6ede068f03a
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2023-12-21 19:31:12 +0530

    add relationship in StudentDetail

--- src/main/java/org/example/entities/StudentCollege.java
+++ src/main/java/org/example/entities/StudentCollege.java
@@ -2,6 +2,8 @@ package org.example.entities;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "STUDENT_COLLEGE")
 public class StudentCollege {
@@ -12,6 +14,21 @@ public class StudentCollege {
 
     @Column(name = "COLLEGE_NAME")
     private String collegeName;
+
+    @OneToMany(mappedBy = "studentCollege",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Student> students;
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     public void setCollegeId(Long collegeId) {
         this.collegeId = collegeId;
     }
